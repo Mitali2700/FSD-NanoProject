3019aa15e720b5b7d208138176f10bb7a1273e65
Shriya-Shandilya <shriya.sh703@gmail.com>
2022-12-10 23:31:09 +0530

    get request generated

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname+ "/index.html")
+})
